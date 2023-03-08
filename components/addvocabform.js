98a63d9d9a89802a98f95aa26e262cd2436ea242
ahamm <ahammer753@gmail.com>
2023-03-07 21:25:32 -0600

    working through error of deleting all cards at same time

--- components/addvocabform.js
+++ components/addvocabform.js
@@ -4,20 +4,20 @@ import renderToDOM from '../utils/renderToDom';
 const addVocabForm = () => {
   clearDom();
   const domString = `
-    <form id="submit-author" class="mb-4">
+    <form id="submit-vocab" class="mb-4">
       <div class="form-group">
         <label for="image">Vocabulary Word</label>
-        <input type="text" class="form-control" id="vocab" placeholder="Example: class " required>
+        <input type="text" class="form-control" id="name" placeholder="Example: class " required>
       </div>
       <div class="form-group">
         <label for="image">Definition</label>
         <input type="text" class="form-control" id="definition" placeholder="Example: Applies a CSS class style to an element " required>
       </div>
       <div class="form-group">
-        <label for="title">Email</label>
+        <label for="title">ltech</label>
         <input type="ltech" class="form-control" id="ltech" aria-describedby="ltech" placeholder="Enter ltech" required>
       </div>
-      <button type="submit" class="btn btn-primary mt-3">Submit Author</button>
+      <button type="submit" class="btn btn-primary mt-3">Submit Vocab</button>
     </form>`;
 
   renderToDOM('#form-container', domString);
--- pages/vocab .js
+++ pages/vocab .js
@@ -1,4 +1,4 @@
-import clearDom from '../utils/clearDom';
+import clearDom from '../utils/cleardom';
 import renderToDOM from '../utils/renderToDom';
 
 const emptyVocab = () => {
@@ -9,21 +9,19 @@ const emptyVocab = () => {
 const showVocab = (array) => {
   clearDom();
 
-  const btnString = '<button class="btn btn-success btn-lg mb-4" id="add-book-btn">Add Vocab</button>';
+  const btnString = '<button class="btn btn-success btn-lg mb-4" id="add-vocab-btn">Add Vocab</button>';
   renderToDOM('#add-button', btnString);
 
   let domString = '';
   array.forEach((item) => {
     domString += `
       <div class="card">
-        <img class="card-img-top" src=${item.image} alt=${item.title} style="height: 400px;">
-        <div class="card-body" style="height: 180px;">
-          <h5 class="card-title">${item.title}</h5>
-            <p class="card-text bold">${item.sale ? `<span class="badge badge-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> $${item.price}` : `$${item.price}`}</p>
+          <h5 class="card-title">${item.name}</h5>
+          <p1 class="card-description">${item.definition}</p1>
+          <p2 class="card-info">${item.tech}</p2>
             <hr>
-            <i class="btn btn-success fas fa-eye" id="view-book-btn--${item.firebaseKey}"></i>
-            <i id="edit-book-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info"></i>
-            <i id="delete-book-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
+            <i id="edit-vocab-btn--${item.firebaseKey}" class="fas fa-edit btn btn-info"></i>
+            <i id="delete-vocab-btn--${item.firebaseKey}" class="btn btn-danger fas fa-trash-alt"></i>
         </div>
       </div>`;
   });
